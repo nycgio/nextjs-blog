bfa1fd3f27d90c2925df641271b3ab2a03d6a8b1
NycGio <giorgialvarez@gmail.com>
2021-10-23 20:36:32 -0400

    Made a custom img renderer for markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
@@ -7,10 +8,31 @@ export default function PostContent(props) {
 	const { post } = props;
 
 	const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+	const renderers = {
+		p(p) {
+			const { node } = p;
+			if (node.children[0].tagName === "img") {
+				const image = node.children[0];
+				return (
+					<div className={classes.image}>
+						<Image
+							src={`/images/posts/${post.slug}/${image.properties.src}`}
+							alt={image.alt}
+							width={600}
+							height={300}
+						/>
+					</div>
+				);
+			}
+			return <p>{p.children}</p>;
+		},
+	};
+
 	return (
 		<article className={classes.content}>
 			<PostHeader title={post.title} image={imagePath} />
-			<ReactMarkdown>{post.content}</ReactMarkdown>
+			<ReactMarkdown components={renderers}>{post.content}</ReactMarkdown>
 		</article>
 	);
 }
